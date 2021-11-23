8dac34a5ee785358cf5c4a581aea8c456ba0e8d9
Shakur Chowdhury <33189134+chowdhuryshakur@users.noreply.github.com>
2021-11-23 23:43:13 +0600

    Add files via upload

---
+++ src/firebase/Firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD_JZU6xADUDeWBJcU9Khvr2PnTH7HiyuA",
+    authDomain: "medisha-3baa3.firebaseapp.com",
+    projectId: "medisha-3baa3",
+    storageBucket: "medisha-3baa3.appspot.com",
+    messagingSenderId: "826511663455",
+    appId: "1:826511663455:web:7d45f7f1bf23b9e044d6ea",
+    measurementId: "G-BZHVBP2NYS"
+};
+
+export default firebaseConfig;
